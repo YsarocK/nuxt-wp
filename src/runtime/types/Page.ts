c09f45bca6ea8fcf22d5f23386a9041d611d411d
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-06 15:49:14 +0100

    improve types

--- src/runtime/types/Page.ts
+++ src/runtime/types/Page.ts
@@ -1,5 +1,63 @@
-export type Page {
-    id: number,
-    acf: any,
-  }
-  
+export type Page = {
+  id: number,
+  date: string,
+  date_gmt: string,
+  guid: {
+      rendered: string
+  },
+  modified: string,
+  modified_gmt: string,
+  slug: string,
+  status: string,
+  type: string,
+  link: string,
+  title: {
+      rendered: string
+  },
+  content: {
+      rendered: string,
+      protected: boolean
+  },
+  excerpt: {
+      rendered: string,
+      protected: boolean
+  },
+  author: number,
+  featured_media: number,
+  parent: number,
+  menu_order: number,
+  comment_status: string,
+  ping_status: string,
+  template: string,
+  meta: {
+      footnotes: string
+  },
+  _links: {
+      self: { href: string }[],
+      collection: { href: string }[],
+      about: { href: string }[],
+      author: {
+          embeddable: boolean,
+          href: string
+      }[],
+      replies: {
+          embeddable: boolean,
+          href: string
+      }[],
+      "version-history": {
+          count: number,
+          href: string
+      }[],
+      "predecessor-version": {
+          id: number,
+          href: string
+      }[],
+      "wp:attachment": { href: string }[],
+      curies: {
+          name: string,
+          href: string,
+          templated: boolean
+      }[]
+  },
+  acf: boolean
+}
--- src/runtime/types/Post.ts
+++ src/runtime/types/Post.ts
@@ -1,8 +1,41 @@
-export type Post {
+export type Post = {
     id: number,
+    date: string,
+    date_gmt: string,
+    guid: {
+        rendered: string
+    },
+    modified: string,
+    modified_gmt: string,
+    slug: string,
+    status: string,
+    type: string,
+    link: string,
     title: {
         rendered: string
     },
-    featured_media?: number,
-    acf: any,
+    content: {
+        rendered: string,
+        protected: boolean
+    },
+    featured_media: number,
+    template: string,
+    realisations_types: any[],
+    _links: {
+        self: { href: string }[],
+        collection: { href: string }[],
+        about: { href: string }[],
+        "wp:attachment": { href: string }[],
+        "wp:term": {
+            taxonomy: string,
+            embeddable: boolean,
+            href: string
+        }[],
+        curies: {
+            name: string,
+            href: string,
+            templated: boolean
+        }[]
+    },
+    acf: boolean
 }
