49bdb4c660e283ee22ac6ac05930bb6ef08ed9f7
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-07 09:30:15 +0100

    handle error useWpPage

--- src/runtime/composables/useWpPage.ts
+++ src/runtime/composables/useWpPage.ts
@@ -1,13 +1,24 @@
 import { useAsyncData } from '#imports'
+import consola from 'consola'
 import type { Page } from '../types'
 
 const useWpPage = async (slug?: string): Promise<Page> => {
+  const route = useRoute()
+  const query = slug || route.path.substring(1)
+
   const { data, error } = await useAsyncData<Array<Page>>('page', async () => {
-    const route = useRoute()
-    const query = slug || route.path.substring(1)
     const { apiEndpoint, additonnalQueryParams } = useRuntimeConfig().public.wordpress
     return $fetch(`${apiEndpoint}/pages?slug=${query}${additonnalQueryParams}`)
   })
+
+  if(error.value) {
+    consola.error(error)
+  }
+
+  if(!data.value || data.value.length === 0) {
+    consola.error(`No page with slug "${query}" found`)
+    return {} as Page
+  }
 
   // @ts-ignore
   return data.value[0] as Page
