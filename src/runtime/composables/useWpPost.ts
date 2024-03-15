25dd36590e338b50e5b054d90c1b0dd779df89fc
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-15 21:32:26 +0100

    fix composable

--- src/runtime/composables/useWpPost.ts
+++ src/runtime/composables/useWpPost.ts
@@ -4,21 +4,17 @@ import type { Post } from '../types'
 
 interface Options {
   type?: string,
-  slug?: string,
+  id?: SVGAnimatedNumberList,
 }
 
-const useWpPost = async ({ type = 'posts', slug }: Options = {}) => {
+const useWpPost = async ({ type = 'posts', id }: Options = {}) => {
   const route = useRoute()
-  const query = slug || route.path.substring(1)
+  const query = id || route.path.substring(1)
 
   const { data, error } = await useAsyncData<Array<Post>>('post', async () => {
     const { apiEndpoint, additonnalQueryParams } = useRuntimeConfig().public.wordpress
-    
-    const params = new URLSearchParams({
-      slug: String(slug),
-    });
   
-    return $fetch(`${apiEndpoint}/${type}?${params.toString()}${additonnalQueryParams}`)
+    return $fetch(`${apiEndpoint}/${type}/${query}${additonnalQueryParams}`)
   })
 
   if(error.value) {
