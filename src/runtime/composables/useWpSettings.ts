d457eb468f937d2e857edbc066fd2bfbf0da5a75
Etienne Moureton <etienne.moureton@unflux.fr>
2024-03-15 22:03:00 +0100

    fix

--- src/runtime/composables/useWpSettings.ts
+++ src/runtime/composables/useWpSettings.ts
@@ -3,7 +3,7 @@ import consola from 'consola'
 import type { Settings } from '../types'
 
 const useWpSettings = async () => {
-  const { data, error } = await useAsyncData<Settings>('post', async () => {
+  const { data, error } = await useAsyncData<Settings>('wp-settings', async () => {
     const { apiEndpoint } = useRuntimeConfig().public.wordpress
 
     return $fetch(`${apiEndpoint}/settings`)
